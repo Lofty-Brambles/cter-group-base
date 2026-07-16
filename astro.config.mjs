82a3023b27af2c3ca8e37a8a1199a1a0172f625b
Prantik Das <prantik.ts@gmail.com>
2026-07-16 12:10:28 +0530

    feat: add base layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,42 @@
 // @ts-check
-import { defineConfig } from "astro/config";
-
+import { defineConfig, fontProviders } from "astro/config";
 import preact from "@astrojs/preact";
-
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact()],
+	fonts: [
+		make_font_provider("Atkinson Hyperlegible"),
+		make_font_provider("Hanken Grotesk"),
+	],
 
-  vite: {
-    plugins: [tailwindcss()],
-  },
+	integrations: [preact()],
+
+	vite: {
+		plugins: [tailwindcss()],
+	},
 });
+
+function make_font_provider(/** @type {string} */ font_name) {
+	const repl_spaces = (
+		/** @type {string} */ string,
+		/** @type {string} */ replacement = "-",
+	) => string.replace(/\s+/g, replacement);
+
+	/** @type {[{ src: [string, ...string[]], weight: string }]} */
+	const variants = [
+		{
+			weight: "100 900",
+			src: [
+				`./src/assets/fonts/${repl_spaces(font_name, "")}.variable.subset.woff2`,
+			],
+		},
+	];
+
+	return {
+		provider: fontProviders.local(),
+		name: font_name,
+		cssVariable: `--font-${repl_spaces(font_name.toLowerCase())}`,
+		options: { variants },
+	};
+}
---
+++ src/env.d.ts
@@ -0,0 +1,7 @@
+interface ImportMetaEnv {
+	readonly PUBLIC_SITE_NAME: string;
+}
+
+interface ImportMeta {
+	readonly env: ImportMetaEnv;
+}
